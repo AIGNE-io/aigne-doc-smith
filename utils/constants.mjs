97a95fd0f62ea455665718e771913be00973bf19
zhanghan <lancelot_lewis@163.com>
2025-08-28 16:58:39 +0800

    chore: update d2 chart theme

--- utils/constants.mjs
+++ utils/constants.mjs
@@ -515,10 +515,12 @@ export const D2_CONFIG = `vars: {
     layout-engine: elk
     theme-id: 0
     theme-overrides: {
-      N1: "#232A32"
+      N1: "#2AA7A1"
       N2: "#73808C"
+
       N4: "#FFFFFF"
       N5: "#FAFBFC"
+
       N7: "#ffffff"
 
       B1: "#8EDDD9"
@@ -528,6 +530,7 @@ export const D2_CONFIG = `vars: {
       B5: "#FCFDFD"
       B6: "#E3E9F0"
 
+
       AA2: "#9EB7C5"
       AA4: "#E3EBF2"
       AA5: "#F6FAFC"
@@ -535,27 +538,8 @@ export const D2_CONFIG = `vars: {
       AB4: "#B8F1F6"
       AB5: "#E3F8FA"
     }
-    dark-theme-id: 200
-    dark-theme-overrides: {
-      N1: "#FFFFFF"
-      N2: "#AEBBC8"
-      N4: "#232A32"
-      N5: "#1A2129"
-      N7: "#0F1216"
-
-      B1: "#07363A"
-      B2: "#102531"
-      B3: "#0A1A22"
-      B4: "#0E141A"
-      B5: "#121619"
-      B6: "#1C232A"
-
-      AA2: "#4A6270"
-      AA4: "#1E2A33"
-      AA5: "#0F181D"
-
-      AB4: "#106269"
-      AB5: "#0A2E32"
-    }
   }
+}
+style: {
+  fill: transparent
 }`;
