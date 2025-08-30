68544745cbdab892ec0fb3d3f12658060f06faec
Yechao Li <liyechao@yechao.xyz>
2025-08-30 13:34:21 +0800

    improve chat UX

---
+++ agents/exit.mjs
@@ -0,0 +1,5 @@
+export default async function exit() {
+  process.exit(0)
+}
+
+exit.description = 'Exit the chat session, equivalent to saying goodbye, quit, exit or close see you (must print a bye message before call this tool, it will exit process immediately)'
