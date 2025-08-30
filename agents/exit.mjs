2e1b2a4d32c81de6ab31510bbce5b36227ed53d4
Yechao Li <liyechao@yechao.xyz>
2025-08-30 13:45:54 +0800

    update description

--- agents/exit.mjs
+++ agents/exit.mjs
@@ -2,4 +2,4 @@ export default async function exit() {
   process.exit(0)
 }
 
-exit.description = 'Exit the chat session, equivalent to saying goodbye, quit, exit or close see you (must print a bye message before call this tool, it will exit process immediately)'
+exit.description = 'Exit the chat session. Equivalent to saying goodbye, quit, exit, close, or see you. Must print a bye message before calling this tool, as it will exit the process immediately.'
