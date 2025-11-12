39632f5193fafbb658efe15c6f8e3eaf9a881b0d
zhang han <LancelotLewis@users.noreply.github.com>
2025-11-12 10:29:07 +0800

    fix: polish openapi spec check condition (#293)

--- utils/openapi/index.mjs
+++ utils/openapi/index.mjs
@@ -1,22 +1,26 @@
 import { parse } from "yaml";
 
+function isMatchOpenAPISpec(content) {
+  if (!content) return false;
+  if (!(content.openapi || content.swagger)) return false;
+  if (!content.paths) return false;
+  if (!(content.info?.title && content.info?.version)) return false;
+  return true;
+}
+
 export function isOpenAPISpecFile(content) {
   const trimmedContent = content.trim();
   try {
     const parsed = parse(trimmedContent, {
       logLevel: "silent",
     });
-    if (parsed.openapi || parsed.swagger) {
-      return true;
-    }
+    return isMatchOpenAPISpec(parsed);
   } catch {
     //
   }
   try {
     const parsed = JSON.parse(trimmedContent);
-    if (parsed.openapi || parsed.swagger) {
-      return true;
-    }
+    return isMatchOpenAPISpec(parsed);
   } catch {
     //
   }
